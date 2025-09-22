377d4bd2eaa7d361e4f5c034b5efe92dfe0f66de
n4k <no4kar@gmail.com>
2025-09-22 18:14:03 +0200

    improve: (.../setup-node-project/cli.sh)

--- scripts/bash/setup-node-project/cli.sh
+++ scripts/bash/setup-node-project/cli.sh
@@ -1,10 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-if [[ ! -f "./v1.sh" ]]; then
-    echo "Error: work file not found."
-    exit 1
+# Get the directory of this script
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)";
+SCRIPT_VERSION="v1.sh";
+SCRIPT_FILE="$SCRIPT_DIR/$SCRIPT_VERSION";
+
+# echo -e "\n$SCRIPT_DIR\n$SCRIPT_FILE"; # -e interpret escape sequences
+
+# Check if v1.sh exists
+if [[ ! -f "$SCRIPT_FILE" ]]; then
+    echo "Error: work file not found at $SCRIPT_FILE";
+    exit 1;
 fi
 
-# echo "$@"
-./v1.sh "$@" # Run v1.sh with all arguments, propagate its exit code
-exit $? # $? holds the exit code of the last command executed
+# Run v1.sh with all arguments, propagate its exit code
+"$SCRIPT_FILE" "$@";
+exit $?; # $? holds the exit code of the last command executed
